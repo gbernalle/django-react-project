4d03b0483358d8b8d27ad417e8ab011de6677619
gbernalle <gabrielbbmatozinhos@gmail.com>
2025-03-11 12:08:24 -0300

    User views created

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,16 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        # we accept a new user and return a new user. (serializer is used in two differents places)
+        extra_kwargs = {"password": {"write_only": True}}
+        # We want to accept password when we creating a new user
+        # We don't want to return password when we givin information of this user
+
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            return user
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    # Verify every objects for indefy that we can create a unique User
+    serializer_class = User
+    # Tells this view what kind of data we need to accept to make new User
+    permission_classes = [AllowAny]
+    # Who can actually call this
